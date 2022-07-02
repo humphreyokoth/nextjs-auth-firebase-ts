f2fd236ed7ef9574e4f996f4817a35d6c1d5ac4b
humphreyokoth <humpasp@gmail.com>
2022-07-02 07:53:08 +0300

    Feat:AuthFirebase

---
+++ config/firabse.ts
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2B3W62hvV2MYbWsEgXLZDxghLSMW5Q4E",
+  authDomain: "nextjs-firebase-auth-33dd7.firebaseapp.com",
+  projectId: "nextjs-firebase-auth-33dd7",
+  storageBucket: "nextjs-firebase-auth-33dd7.appspot.com",
+  messagingSenderId: "943640030301",
+  appId: "1:943640030301:web:c9802c9824c620f0e0b1b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
