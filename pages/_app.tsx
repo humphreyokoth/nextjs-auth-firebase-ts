6b6c1868f8772d51df7ce4e81db8db486b13949d
humphreyokoth <humpasp@gmail.com>
2022-07-04 15:14:41 +0300

    Feat:App protected route

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,26 @@ import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from '../components/Navbar';
 import { AuthContextProvider } from '../context/AuthContext';
+import {useRouter} from 'next/router';
+import ProtectedRoute from '../components/ProtectedRoute';
 
+
+  const noAuthRequired = ['/','/login','/signup']
 
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  const router = useRouter()
   return (
   <AuthContextProvider>
   <Navbar/>
-  <Component {...pageProps} />
+  {noAuthRequired.includes(router.pathname) ? (
+  <Component {...pageProps} />) :(
+    <ProtectedRoute>
+      <Component {...pageProps} />
+    </ProtectedRoute>
+    
+  )}
   </AuthContextProvider>
   )
 }
