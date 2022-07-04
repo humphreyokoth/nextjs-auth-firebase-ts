c084b2ed384e71ec3167e861eb3f382b1138a0c2
humphreyokoth <humpasp@gmail.com>
2022-07-04 10:25:57 +0300

    Fix:Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,14 +1,32 @@
+import Link from 'next/link';
 import React from 'react'
-import { Container ,Nav,Navbar} from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
-const Navbar = () => {
-  return (
-    <Navbar bg='light' expand='lg'>
-<Container>
 
-</Container>
-    </Navbar>
-  )
+const NavbarComp = () => {
+    return (
+
+        <Navbar bg='light' expand='lg'>
+            <Container>
+                <Link href='/' passHref>
+                    <Navbar.Brand>Firebase Auth</Navbar.Brand>
+                </Link>
+                <Navbar.Toggle aria-controls='basic-navbar-nav'/>
+                <Navbar.Collapse id= 'basic-navbar-nav'>
+                    <Nav className='me-auto'>
+                        <Link href='/signup' passHref>
+                            <Nav.Link>Signup</Nav.Link>
+                        </Link>
+                        <Link href='/login' passHref>
+                            <Nav.Link>Login</Nav.Link>
+                        </Link>
+                    </Nav>
+
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    )
 }
 
-export default Navbar;
+export default NavbarComp;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from '../components/Navbar';
+
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Navbar/>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
