89167b8f963778c105cc1efdcc592e6180b4ff77
humphreyokoth <humpasp@gmail.com>
2022-07-04 10:51:46 +0300

    Feat:AuthContext

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -8,12 +8,21 @@ export const  AuthContextProvider =({children}:{children:React.ReactNode })=>{
     const [user,setUser] = useState<any>(null)
 useEffect(()=>{
     const unsubscribe = onAuthStateChanged(auth,(user)=>{
-        
+        if(user){
+            setUser({
+                uid:user.uid,
+                email:user.email,
+                displayName:user.displayName,
+            })
+        }else{
+            setUser(null)
+        }
+        return ()=> unsubscribe()
 
     })
 },[])
     return (
-        <AuthContext.Provider value={{}}>
+        <AuthContext.Provider value={{user}}>
             {children}
         </AuthContext.Provider>
     )
